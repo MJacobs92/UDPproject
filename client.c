bd5d51e3aa0e33e92b87751413b60297ea27d584
MJacobs92 <marco@marco>
2017-02-09 22:29:15 -0500

    created funtion that will send message to server. IT will only print the right message at the momemt.

--- client.c
+++ client.c
@@ -1,19 +1,27 @@
 #include "stdio.h"
+#include <string.h>
 
 int main() 
 {
+	// action that the user wants to perform
    char inputAction;
+   //string that the user wants capitalized
+   char userString[50]
    
+   //keep looping until the user decides to quit the program
    while(inputAction != 'q') 
    {
    	printf("Here are your allowed commands:\n s - to enter string to capitalize.\n t - to get a file\n q - to quit\n Enter your command: ");
    	
    	scanf(" %c",&inputAction);
 
+   	//choose an action to perform based on what the user input
    switch(inputAction) {
 
    	case 's':
-   	printf("You entered s\n");
+
+   	printf("Please enter a string to capitalize\n");
+   	scanf(" %s",&userString);
    		break;
    	case 't':
    	    printf("You entered t\n");
@@ -23,4 +31,16 @@ int main()
 }
 
    return 0;
+}
+
+void capitalizeStr(char *userString)
+{
+	// create a string that is big enough to hold the message to send to the server 
+    char *message = malloc(strlen(userString)+5);
+    
+    // create the string to send the server.
+    strcpy(message, "CAP\n");
+    strcat(message, userString);
+    strcat(message, "\n");
+    printf("%s",message);
 }
