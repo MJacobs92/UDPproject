0852977063fbd6f6116fd1eb73741934fa63dd5b
MJacobs92 <marco@marco>
2017-02-10 11:08:42 -0500

    added server connection info and created udp socket

--- server.c
+++ server.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(int argc, char **argv) 
 {
 	// read server udp port from command line and store
 	char* udpPort = argv[1];
-
 	printf("udp: %s\n", udpPort);
+
+	int socket;
+
+	struct sockaddr_in servaddr;
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+	servaddr.sin_port = htons(udpPort);
+
+    socket = socket(PF_INET, SOCK_DGRAM, 0);
 }
