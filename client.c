5a631d862665500470654a94c37927d19f78b37d
MJacobs92 <marco@marco>
2017-02-09 22:59:48 -0500

    created function to get files. only prints message that it will send to server for now

--- client.c
+++ client.c
@@ -14,12 +14,28 @@ void capitalizeStr(char *messageString)
     printf("%s",message);
 }
 
+void recieveFile(char *messageString)
+{
+	// create a string that is big enough to hold the message to send to the server 
+    char *message = malloc(strlen(messageString)+6);
+    
+    // create the string to send the server.
+    strcpy(message, "FILE\n");
+    strcat(message, messageString);
+    strcat(message, "\n");
+    printf("%s",message);
+}
+
 int main() 
 {
 	// action that the user wants to perform
    char inputAction;
+   
    //string that the user wants capitalized
    char userString[50];
+
+   //file name entered by user
+   char fileName[50];
    
    //keep looping until the user decides to quit the program
    while(inputAction != 'q') 
@@ -32,13 +48,14 @@ int main()
    switch(inputAction) {
 
    	case 's':
-
-   	printf("Please enter a string to capitalize: ");
-   	scanf(" %s",userString);
-   	capitalizeStr(userString);
+	   	printf("Please enter a string to capitalize: ");
+	   	scanf(" %s",userString);
+	   	capitalizeStr(userString);
    		break;
    	case 't':
-   	    printf("You entered t\n");
+   	    printf("Please enter a file name to recieve: ");
+	   	scanf(" %s",fileName);
+	   	recieveFile(fileName);
    		break;
    }
 
